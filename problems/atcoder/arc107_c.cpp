79cae0b28c7e52c5ac4992d83d72a734b94687e3
Maria <mlovatonv@gmail.com>
2020-11-08 16:53:21 -0500

    Replace tabs

--- problems/atcoder/arc107_c.cpp
+++ problems/atcoder/arc107_c.cpp
@@ -26,20 +26,20 @@ struct UnionFind {
   }
   
   bool issame(int x, int y) {
-  	return root(x) == root(y);
+    return root(x) == root(y);
   }
   
   bool merge(int x, int y) {
-  	x = root(x); y = root(y);
-  	if (x == y) return false;
-  	if (par[x] > par[y]) swap(x, y); // merge technique
-  	par[x] += par[y];
-  	par[y] = x;
-  	return true;
+    x = root(x); y = root(y);
+    if (x == y) return false;
+    if (par[x] > par[y]) swap(x, y); // merge technique
+    par[x] += par[y];
+    par[y] = x;
+    return true;
   }
   
   int size(int x) {
-  	return -par[root(x)];
+    return -par[root(x)];
   }
 };
 
@@ -47,72 +47,72 @@ struct UnionFind {
 template<int MOD> struct Fp {
   long long val;
   constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
-  	if (val < 0) val += MOD;
+    if (val < 0) val += MOD;
   }
   constexpr int getmod() const { return MOD; }
   constexpr Fp operator - () const noexcept {
-  	return val ? MOD - val : 0;
+    return val ? MOD - val : 0;
   }
   constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
   constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
   constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
   constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
   constexpr Fp& operator += (const Fp& r) noexcept {
-  	val += r.val;
-  	if (val >= MOD) val -= MOD;
-  	return *this;
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
   }
   constexpr Fp& operator -= (const Fp& r) noexcept {
-  	val -= r.val;
-  	if (val < 0) val += MOD;
-  	return *this;
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
   }
   constexpr Fp& operator *= (const Fp& r) noexcept {
-  	val = val * r.val % MOD;
-  	return *this;
+    val = val * r.val % MOD;
+    return *this;
   }
   constexpr Fp& operator /= (const Fp& r) noexcept {
-  	long long a = r.val, b = MOD, u = 1, v = 0;
-  	while (b) {
-  	  long long t = a / b;
-  	  a -= t * b, swap(a, b);
-  	  u -= t * v, swap(u, v);
-  	}
-  	val = val * u % MOD;
-  	if (val < 0) val += MOD;
-  	return *this;
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
   }
   constexpr bool operator == (const Fp& r) const noexcept {
-  	return this->val == r.val;
+    return this->val == r.val;
   }
   constexpr bool operator != (const Fp& r) const noexcept {
-  	return this->val != r.val;
+    return this->val != r.val;
   }
   friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
-  	is >> x.val;
-  	x.val %= MOD;
-  	if (x.val < 0) x.val += MOD;
-  	return is;
+    is >> x.val;
+    x.val %= MOD;
+    if (x.val < 0) x.val += MOD;
+    return is;
   }
   friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
-  	return os << x.val;
+    return os << x.val;
   }
   friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
-  	if (n == 0) return 1;
-  	if (n < 0) return modpow(modinv(r), -n);
-  	auto t = modpow(r, n / 2);
-  	t = t * t;
-  	if (n & 1) t = t * r;
-  	return t;
+    if (n == 0) return 1;
+    if (n < 0) return modpow(modinv(r), -n);
+    auto t = modpow(r, n / 2);
+    t = t * t;
+    if (n & 1) t = t * r;
+    return t;
   }
   friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
-  	long long a = r.val, b = MOD, u = 1, v = 0;
-  	while (b) {
-  	  long long t = a / b;
-  	  a -= t * b, swap(a, b);
-  	  u -= t * v, swap(u, v);
-  	}
-  	return Fp<MOD>(u);
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b, swap(a, b);
+      u -= t * v, swap(u, v);
+    }
+    return Fp<MOD>(u);
   }
 };
 
@@ -161,13 +161,13 @@ mint ans1,ans2;
 mint solve(){
   UnionFind u(n);
   rep(i,0,n){
-  	rep(j,i+1,n){
-  		ok=true;
-  		rep(m,0,n){
-  			if(a[m][i]+a[m][j]>k)ok=false;
-  		}
-  		if(ok)u.merge(i,j);
-  	}
+    rep(j,i+1,n){
+        ok=true;
+        rep(m,0,n){
+            if(a[m][i]+a[m][j]>k)ok=false;
+        }
+        if(ok)u.merge(i,j);
+    }
   }
   mint ans=1;
   //get groups
